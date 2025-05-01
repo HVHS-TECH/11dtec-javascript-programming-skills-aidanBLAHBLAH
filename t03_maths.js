44a2ec671741ec2e183e66048fbb848d55d2413d
aidanBLAHBLAH <23201as@hvhs.school.nz>
2025-05-01 01:29:13 +0000

    ok

---
+++ t03_maths.js
@@ -0,0 +1,39 @@
+/************************************************************************************************************
+ Name of task: Maths in javascript 
+ Person who rote it: Aidan Shannon                             
+ Date written: 1/5/2025                                       
+ Purpose: Doing maths in javascript              
+ ***********************************************************************************************************/
+ console.log("Running t03_maths.js");
+
+ //Variables
+ var num1 = 10;
+ var num2 = 5;
+ var answer;
+ 
+ /************************************************************************************************************ 
+  Main code 
+  ***********************************************************************************************************/
+ answer = num1 + num2;
+ console.log("add", answer);
+ answer = num1 - num2;
+ console.log("subtract", answer);
+ answer = num1 * num2;
+ console.log("multiply", answer);
+ answer = num1 / num2;
+ console.log("divide", answer);
+ answer = ((64 / 16) * (2 + 4) / 8) * 5;
+ console.log("brackets", answer);
+ console.log("Hi Aidan");
+ console.log("As of 1/5/2025 you are 15 years old");
+ answer =  (20 * 100) + 9;
+ console.log("You were born in", answer);
+ answer = 5 + 5 + 5 + 5 + 5;
+ console.log("In 10 years you will be", answer, "years old");
+ let pocketMoney = 10000000000000000000000000000000000;
+ console.log("You have", pocketMoney, "dollars");
+ answer = 10000000000000000000000000000000000 / 2;
+ console.log("Half your money is", answer);
+ /************************************************************************************************************
+  Functions 
+  ***********************************************************************************************************/
