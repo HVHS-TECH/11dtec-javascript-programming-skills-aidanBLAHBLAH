804812dbd47fc1b03e057ae97c068bf6fb06bd60
aidanBLAHBLAH <23201as@hvhs.school.nz>
2025-05-01 02:07:21 +0000

    May 1, 2025, 2:07 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -20,7 +20,7 @@ var perimeter = 2 + length + 2 * width;
   Main code 
   ***********************************************************************************************************/
 // Display the results
-console.log("A area of a piece of paper with a length of", length, "and a width of ", width +" is "+ area);
+console.log("A area of a piece of paper with a length of", length, "and a width of ", width, " is ", area);
 console.log("The same piece of paper has a perimeter of "+ perimeter);
 
 // Check, does this result look sensible
