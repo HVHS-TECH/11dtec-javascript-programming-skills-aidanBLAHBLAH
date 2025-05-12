f7cc71e361d82300e168cffcbf453957e280e47e
aidanBLAHBLAH <23201as@hvhs.school.nz>
2025-05-12 21:56:01 +0000

    okokokokokoko

---
+++ t09_functions.js
@@ -0,0 +1,45 @@
+/************************************************************************************************************
+ Name of task: Functions
+ Person who rote it: Aidan Shannon                             
+ Date written: 29/4/2025                                       
+ Purpose: How to use functions              
+ ***********************************************************************************************************/
+console.log("Running t09_functions.js");
+
+//Variables
+
+
+/************************************************************************************************************ 
+ Main code 
+ ***********************************************************************************************************/
+function getFavColor(){
+    userFavColor = prompt("What is your favourite color?");
+    alert ("Your favourite color is: " + userFavColor);
+};
+getFavColor();
+
+function getUserName(){
+    userabcdefghijlkmnopqrstuvwxyz = prompt("What's your name?")
+    alert("Welcome " + userabcdefghijlkmnopqrstuvwxyz);
+};
+getUserName();
+
+function askUserAge(){
+    userAge = prompt("Who old are you?");
+    alert("Welcome " + userAge);
+};
+askUserAge();
+
+function askUserMoney(){
+   var userMoney = prompt("How much money do you have?")
+    if (userMoney < 4){
+        alert("Your too broke to buy chocolate");
+    };
+    if (userMoney >= 4){
+        alert("You can buy chocolate");
+    };
+};
+askUserMoney();
+/************************************************************************************************************
+ Functions 
+ ***********************************************************************************************************/
