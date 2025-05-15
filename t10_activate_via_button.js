b5fc1f4534481fb05cfe7d16382ce2c9c5523dbc
aidanBLAHBLAH <23201as@hvhs.school.nz>
2025-05-15 00:46:54 +0000

    May 15, 2025, 12:46 AM

--- t10_activate_via_button.js
+++ t10_activate_via_button.js
@@ -9,22 +9,22 @@ console.log("Running t10_activate_via_button.js");
 //Variables
 let userName;
 let userAge;
-let userMoney;
+let userMoney =;
 
-function start(){
-   userName = askName;
-   userAge = askuserAge;
-   userMoney = askMoney;
-    }
-    function askName(){
-        prompt("How old are you?")
-    }
-    function askuserAge(){
-        prompt("How old are you?")
-    }
-    function askMoney(){
-        prompt("How much money do you have?")
-    }
+function start() {
+    userName = askName;
+    userAge = askuserAge;
+    userMoney = askMoney;
+}
+function askName() {
+    prompt("How old are you?")
+}
+function askuserAge() {
+    prompt("How old are you?")
+}
+function askMoney() {
+    prompt("How much money do you have?")
+}
 /************************************************************************************************************ 
  Main code 
  ***********************************************************************************************************/
