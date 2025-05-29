a58d00bfa1c951ac3b789433805b34a09c5a8b1c
aidanBLAHBLAH <23201as@hvhs.school.nz>
2025-05-29 00:47:48 +0000

    a

--- t13_validations.js
+++ t13_validations.js
@@ -5,21 +5,31 @@
  Purpose: How to work validation            
  ***********************************************************************************************************/
 console.log("Running t13_validation.js");
-
 //Variables
 var pocketMoney = prompt("How much pocket money do you have?");
 let nooo = true;
-
 /************************************************************************************************************ 
  Main code 
  ***********************************************************************************************************/
 while (nooo) {
     if(pocketMoney == null || pocketMoney == "" || pocketMoney == " " || isNaN(pocketMoney)) {
         alert("Enter a valid number");
-    }else (pocketMoney == 1){
-        alert("you can buy a chocolate")
     }
-
+    if (pocketMoney == 1);{
+        alert("you can buy a Mars bar");
+    }
+    if (pocketMoney == 2){
+        alert("You can buy a Moro bar")
+    }
+    if (pocketMoney == 3){
+        alert("You can buy a King Size Country")
+    }
+    if (pocketMoney == 4){
+        alert("You can buy a Dairy Mint Block")
+    }
+    if (pocketMoney == 5){
+        alert("You can buy a Lindt Block")
+    }
 /************************************************************************************************************
  Functions 
  ***********************************************************************************************************/
