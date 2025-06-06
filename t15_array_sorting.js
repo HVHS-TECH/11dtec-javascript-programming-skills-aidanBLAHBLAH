773945e7ee78f1186baf6bf18e5ca7390823b9c9
aidanBLAHBLAH <23201as@hvhs.school.nz>
2025-06-06 02:48:45 +0000

    Jun 6, 2025, 2:48 AM

--- t15_array_sorting.js
+++ t15_array_sorting.js
@@ -7,7 +7,6 @@
 console.log("Running t01_introduction.js");
 
 //Variables
-let userName;
 let pocketMoney;
 let truePocket = true;
 let trueName = true;
@@ -18,15 +17,17 @@ let shopMessage = "Welcome to the shop, you can buy";
  Main code 
  ***********************************************************************************************************/
 while (trueName) {
-    userName = prompt("What's your name?")
+   let userName = prompt("What's your name?")
     if(userName == null || userName == "" || userName == " " || isNaN) {
         alert("Enter a name");
-    }else{
+    }
+
+    
 for (i=0;i<chocolateArray.length;i++){
   shopMessage = shopMessage + "\nA "+chocolateArray[i]+" is "+chocolatePrices[i];
- }}
+ }
  alert(shopMessage)
-}
+
   while (truePocket) {
     pocketMoney = prompt("How much pocket money do you have? (In dollars)")
     if(pocketMoney == null || pocketMoney == "" || pocketMoney == " " || isNaN(pocketMoney)) {
