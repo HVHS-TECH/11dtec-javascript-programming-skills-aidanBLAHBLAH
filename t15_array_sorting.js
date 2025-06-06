9dc8e4d6032a3721451221b42ba0fbbf187eb263
aidanBLAHBLAH <23201as@hvhs.school.nz>
2025-06-06 02:35:28 +0000

    Jun 6, 2025, 2:35 AM

--- t15_array_sorting.js
+++ t15_array_sorting.js
@@ -7,18 +7,36 @@
 console.log("Running t01_introduction.js");
 
 //Variables
+let userName;
 let pocketMoney;
-let nooo = true;
+let truePocket = true;
+let trueName = true;
 let chocolateArray = ["Mars bar", "Moro bar", "King sized crunchy", "Dairy milk block", "Lindt block" ];
 let chocolatePrices = ["$2", "$2.50", "$3", "$6", "$7"];
 let shopMessage = "Welcome to the shop, you can buy";
 /************************************************************************************************************ 
  Main code 
  ***********************************************************************************************************/
+while (trueName) {
+    userName = prompt("What's your name?")
+    if(userName == null || userName == "" || userName == " " || isNaN(userName)) {
+        alert("Enter a name");
+    }
+    
+  }
+
 for (i=0;i<chocolateArray.length;i++){
   shopMessage = shopMessage + "\nA "+chocolateArray[i]+" is "+chocolatePrices[i];
  }
  alert(shopMessage)
+
+  while (truePocket) {
+    pocketMoney = prompt("How much pocket money do you have? (In dollars)")
+    if(pocketMoney == null || pocketMoney == "" || pocketMoney == " " || isNaN(pocketMoney)) {
+        alert("Enter a valid number");
+    }
+
+  }
 /************************************************************************************************************
  Functions 
  ***********************************************************************************************************/
