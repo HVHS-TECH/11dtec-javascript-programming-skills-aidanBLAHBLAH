91e37d911149ef7825f4906c09c010ea62c1bf8e
aidanBLAHBLAH <23201as@hvhs.school.nz>
2025-06-13 02:34:21 +0000

    Jun 13, 2025, 2:34 AM

--- t15_array_sorting.js
+++ t15_array_sorting.js
@@ -14,11 +14,15 @@ let truePocket = true;
 let trueName = true;
 let chocolateArray = ["Mars bar", "Moro bar", "King sized crunchy", "Dairy milk block", "Lindt block" ];
 let chocolatePrices = ["$2", "$2.50", "$3", "$6", "$7"];
-let shopMessage = "Welcome to the shop, " + userName +  " you can buy";
+let shopMessage = "Welcome to the shop"
 /************************************************************************************************************ 
  Main code 
  ***********************************************************************************************************/
-while (trueName) {
+      for (i=0;i<chocolateArray.length;i++){
+  shopMessage = shopMessage + "\nA "+ chocolateArray[i] +" is "+chocolatePrices[i];
+ }
+
+while(trueName){
     userName = prompt("What's your name?")
     if(userName == null || userName == "" || userName == " "||isNaN(false)) {
         alert("Enter a name");
@@ -26,24 +30,28 @@ while (trueName) {
       userAge = prompt("How old are you?")
     }if(userAge == null || userAge == "" || userAge == " " || isNaN(userAge)){
       alert("Enter a number")
-    }if(isNaN(false)){
+    }if(isNaN){
       alert(shopMessage)
-    }}
-
-
-      for (i=0;i<chocolateArray.length;i++){
-  shopMessage = shopMessage + "\nA "+chocolateArray[i]+" is "+chocolatePrices[i];
- }
- 
-    
-  
-
-  while (truePocket) {
+    }
     pocketMoney = prompt("How much pocket money do you have? (In dollars)")
     if(pocketMoney == null || pocketMoney == "" || pocketMoney == " " || isNaN(pocketMoney)) {
         alert("Enter a valid number");
+    }if(pocketMoney < 2){
+alert("You cannot afford any chocolate")
+    }if(pocketMoney == 2){
+      alert("You can buy a mars bar")
     }
-  }
+    if(pocketMoney == 2.50){
+      alert("You can buy a Moro bar")
+    }if (pocketMoney == 3){
+      alert("You can buy a king sized crunchy")
+    }if(pocketMoney == 6){
+      alert("You can buy a Dairy Milk block")
+    }if(pocketMoney >= 7){
+      alert("You can buy a Lindt block")
+    }}
+  
+
 /************************************************************************************************************
  Functions 
  ***********************************************************************************************************/
